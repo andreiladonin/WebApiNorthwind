a116bb22e02b008734245a6c1f4e9af58773e5ee
Andrei Ladonin <ladonin2001@outlook.com>
2021-10-24 20:14:22 +0400

    append js method Post

--- WebApiNorthwind/wwwroot/script/script.js
+++ WebApiNorthwind/wwwroot/script/script.js
@@ -16,7 +16,9 @@ getCategories(URL)
 
     data.forEach(element => {
         /* console.log (element["name"]  + " " + element['desc']);  */
-        
+        let tdId = document.createElement("td");
+        tdId.innerText = element["id"];
+
         let tdName = document.createElement("td");
         tdName.innerText = element["name"];
 
@@ -24,9 +26,43 @@ getCategories(URL)
         tdDesc.innerText = element["desc"];
 
         let tr = document.createElement("tr");
+        tr.append(tdId)
         tr.append(tdName);
         tr.append(tdDesc);
 
         tbody.append(tr);
     });
 });
+
+async function append_category(categories) {
+    let response = await fetch(URL, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json;charset=utf-8'
+          },
+        body: JSON.stringify(categories)
+    });
+    let result = await response.json();
+    return result;
+}
+
+document.querySelector("#btn-submit").addEventListener("click", () => {
+
+     let name_category = document.querySelector("#name_category").value;
+     let desc_category = document.querySelector("#desc_category").value;
+    
+     let categories = {
+         "CategoryName": name_category,
+         "Description": desc_category
+     }
+     
+     if(name_category == "" || desc_category == "") {
+         alert("Введите значения");
+     } else{
+
+        append_category(categories).then((result)=> {
+            alert("ok");
+            window.location.reload();
+        })
+     }
+})
